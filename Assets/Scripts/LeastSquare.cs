867028c05d23a19716694d3adb49fb278d9d4ced
Eduardo Teixeira de Souza <lkledu@gmail.com>
2019-05-18 08:01:23 -0300

    leastSquare class

---
+++ Assets/Scripts/LeastSquare.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeastSquare : MonoBehaviour
+{
+    protected List<int> x_table;
+    protected List<int> y_table;
+    private List<int> x_square_table;
+    private List<int> xy_table;
+
+    void populateXSquare() {
+
+        for (int i = 0; i < x_table.Count; i++)
+        {
+            x_square_table.Add(x_table[i] * x_table[i]);
+        }
+    }
+
+    void populateXY()
+    {
+
+        for (int i = 0; i < x_table.Count; i++)
+        {
+            xy_table.Add(x_table[i] * y_table[i]);
+        }
+    }
+
+    int somatoria(List<int> coluna) {
+        int result = 0;
+        for (int i = 0; i < coluna.Count; i++)
+        {
+            result =+ coluna[i];
+        }
+
+        return result;
+    }
+
+
+    int determinante() {
+        int delta = 0;
+        int[,] delta_matriz = new int[2,2];
+        delta_matriz[0, 0] = x_table.Count;
+        delta_matriz[0, 1] = somatoria(x_table);
+        delta_matriz[1, 0] = somatoria(x_table);
+        delta_matriz[1, 1] = somatoria(x_square_table);
+
+        delta = (-1 * (delta_matriz[0, 0] * delta_matriz[1, 1]) + (delta_matriz[0, 1] * delta_matriz[1, 0]));
+
+        return delta;
+    }
+
+    int determinanteA() {
+        int delta = 0;
+        int[,] delta_matriz = new int[2, 2];
+        delta_matriz[0, 0] = somatoria(y_table);
+        delta_matriz[0, 1] = somatoria(xy_table);
+        delta_matriz[1, 0] = somatoria(x_table);
+        delta_matriz[1, 1] = somatoria(x_square_table);
+
+        delta = (-1 * (delta_matriz[0, 0] * delta_matriz[1, 1]) + (delta_matriz[0, 1] * delta_matriz[1, 0]));
+
+        return delta;
+    }
+
+    int determinanteB() {
+        int delta = 0;
+        int[,] delta_matriz = new int[2, 2];
+        delta_matriz[0, 0] = somatoria(y_table);
+        delta_matriz[0, 1] = somatoria(xy_table);
+        delta_matriz[1, 0] = somatoria(y_table);
+        delta_matriz[1, 1] = somatoria(xy_table);
+
+        delta = (-1 * (delta_matriz[0, 0] * delta_matriz[1, 1]) + (delta_matriz[0, 1] * delta_matriz[1, 0]));
+
+        return delta;
+    }
+
+    float lastSquareResult() {
+        float y = 0, a, b;
+
+        a = determinanteA() / determinante();
+        b = determinanteB() / determinante();
+
+        y = a+b;
+
+        return y;
+    }
+}
