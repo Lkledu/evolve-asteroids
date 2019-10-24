449060916cd4f5d208e676b3ebbadb9c3fb46585
Eduardo Teixeira de Souza <lkledu@gmail.com>
2019-10-24 01:42:56 -0200

    rollback to old fitness calculation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -383,7 +383,7 @@ public class GameManager : MonoBehaviour
 
     public double EvaluationFitness()
     {
-        return m_lSquare.yHat();
+        return m_Score;
     }
 
     public void DecrementLives()
